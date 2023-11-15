067c14c711a6766a3cd172464aaf282f83c4b65a
Asliddin9220 <asliddinmalikov92@gmail.com>
2023-11-15 05:33:29 +0500

    first commit

--- blog/post/models.py
+++ blog/post/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 class Post(models.Model):
     title = models.CharField(max_length=100)
+    id = models.AutoField(primary_key=True)
     created_date = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
