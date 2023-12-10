bd0a026dc4daf7a687a6ded7b3db453540869f59
Asliddin9220 <asliddinmalikov92@gmail.com>
2023-12-10 06:26:19 +0500

    first commit

--- blog/post/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from .models import Post
-
-def post_list(request):
-    posts = Post.objects.all()
-    for post in posts:
-        print(post.image)
-    return render(request,'post_list.html', {'posts':posts})
-# Create your views here.
